caeefca94448649de0125cebf1840d388271014a
Edmund Wu <fangkazuto@gmail.com>
2020-04-04 13:19:23 -0400

    inherit dependency outputs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   edition = 201909;
   description = "A collection of Nix flakes";
 
-  outputs = { self, nixpkgs }:
-    {
-      #
-    };
+  inputs.rolling.url = "github:eadwu/flakes/rolling";
+
+  outputs = inputs@{ self, nixpkgs, rolling }:
+   with nixpkgs.lib; foldl recursiveUpdate {} (attrValues (removeAttrs inputs [ "self" "nixpkgs" ]));
 }
