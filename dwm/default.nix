431ab5a73141d15f58dc44415960eeb31564be62
Edmund Wu <fangkazuto@gmail.com>
2020-04-05 11:38:49 -0400

    dwm: 2020-04-03 -> 2020-03-22

--- dwm/default.nix
+++ dwm/default.nix
@@ -3,12 +3,12 @@
 dwm.overrideAttrs (_: rec {
   name = "${pname}-${version}";
   pname = "dwm";
-  version = "2020-04-03";
+  version = "2020-03-22";
 
   src = fetchFromGitLab {
     owner = "eadwu";
     repo = "dwm";
-    rev = "";
-    sha256 = "1q8sl55qqaizdb941pj759wcdpykvpwmbfvcfciw7sahgg1l21f1";
+    rev = "991663dae7e73322cd87b98bc723e3f3c360c536";
+    sha256 = "1r3a8161hn9xa5q8di1w3fll9q0x0r620vm6dj2c0pi0hkakhm8j";
   };
 })
