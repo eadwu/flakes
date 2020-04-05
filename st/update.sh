b6a60fce23609ce93fbdba2f4ca31bb2fcfc2de9
Edmund Wu <fangkazuto@gmail.com>
2020-04-05 11:41:22 -0400

    dwm,st: update using correct information

--- st/update.sh
+++ st/update.sh
@@ -4,6 +4,7 @@
 current_dir="$(pwd)/$(dirname ${BASH_SOURCE[0]})"
 root="$(readlink -f "$current_dir/..")"
 attr="st"
+branch="develop"
 
 drv="$root#packages.x86_64-linux.$attr"
 old_version="$(nix show-derivation "$drv" | jq -r '.[].env.version')"
@@ -15,8 +16,8 @@ repo_src="$(nix show-derivation "$old_src_drv" | jq -r '.[].env.urls' | grep -Po
 tmpdir="$current_dir/result"
 git clone --quiet "https://gitlab.com/$repo_src.git" "$tmpdir"
 
-new_version="$(cd $tmpdir && git log -1 --format=%cs)"
-new_revision="$(cd "$tmpdir" && git rev-parse HEAD)"
+new_version="$(cd $tmpdir && git log -1 --format=%cs "origin/$branch")"
+new_revision="$(cd "$tmpdir" && git rev-parse "origin/$branch")"
 new_hash="$(nix-prefetch-git "https://gitlab.com/$repo_src.git" --rev "$new_revision" --quiet | jq -r '.sha256')"
 
 rm -rf "$tmpdir"
@@ -24,12 +25,11 @@ rm -rf "$tmpdir"
 # https://github.com/NixOS/nixpkgs/blob/0e6ceb87584feec4836975d8216f9ca4c2199e0e/pkgs/common-updater/scripts/update-source-version#L108
 # Escape regex metacharacter that are allowed in store path names
 old_version_escaped="$(echo "$old_version" | sed -re 's|[.+]|\\&|g')"
-old_rev_escaped="$(echo "$old_rev" | sed -re 's|[.+]|\\&|g')"
 
 source_file="$current_dir/default.nix"
 sed -i "$source_file" -re "s|\"$old_version_escaped\"|\"$new_version\"|"
 sed -i "$source_file" -re "s|\"$old_hash\"|\"$new_hash\"|"
-sed -i "$source_file" -re "s|\"$old_rev_escaped\"|\"$new_rev\"|"
+sed -i "$source_file" -re "s|\"$old_rev\"|\"$new_revision\"|"
 
 if [[ "$new_version" != "$old_version" ]]; then
   git add "$source_file"
