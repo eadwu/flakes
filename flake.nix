9d5b9acf2e9193cffeefd25547d3982677b2fd00
Edmund Wu <fangkazuto@gmail.com>
2020-04-05 22:39:48 -0400

    include outputs from branch "custom"

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   edition = 201909;
   description = "A collection of Nix flakes";
 
+  inputs.custom.url = "github:eadwu/flakes/custom";
   inputs.rolling.url = "github:eadwu/flakes/rolling";
 
   outputs = inputs@{ self, nixpkgs, rolling }: with nixpkgs.lib;
