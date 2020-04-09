a0be1821a11354e682272ba7c8f97a0263516076
Edmund Wu <fangkazuto@gmail.com>
2020-04-09 18:08:37 -0400

    flake.nix: use new input attrset syntax

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   edition = 201909;
   description = "A collection of Nix flakes";
 
-  inputs.custom.uri = github:eadwu/flakes/custom;
-  inputs.rolling.uri = github:eadwu/flakes/rolling;
+  inputs.custom = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "custom"; };
+  inputs.rolling = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "rolling"; };
 
   outputs = { self, nixpkgs, ... }@inputs: with nixpkgs.lib;
     foldl recursiveUpdate {}
