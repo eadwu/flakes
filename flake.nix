e94485403aec89cec53f6a55e42415d82420f378
Edmund Wu <fangkazuto@gmail.com>
2020-04-09 18:13:34 -0400

    flake.nix: cleanly merge imported overlays

--- flake.nix
+++ flake.nix
@@ -5,7 +5,11 @@
   inputs.custom = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "custom"; };
   inputs.rolling = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "rolling"; };
 
-  outputs = { self, nixpkgs, ... }@inputs: with nixpkgs.lib;
-    foldl recursiveUpdate {}
-      (map (v: v.outputs) (attrValues (removeAttrs inputs [ "self" "nixpkgs" ])));
+  outputs = { self, nixpkgs, ... }@inputs: with nixpkgs.lib; let
+    _foldl' = f: foldl recursiveUpdate {} (map f (attrValues (removeAttrs inputs [ "self" "nixpkgs" ])));
+  in
+    _foldl' (f: f.outputs)
+    // {
+      overlays = system: self: super: _foldl' (f: f.overlays system self super);
+    };
 }
