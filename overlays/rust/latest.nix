53bf1ed88165cccca0f843a74bf2d4b51cfe8357
Edmund Wu <fangkazuto@gmail.com>
2020-04-10 17:27:05 -0400

    nixpkgs-fmt

--- overlays/rust/latest.nix
+++ overlays/rust/latest.nix
@@ -1,13 +1,15 @@
 { nixpkgs-mozilla }:
-
 let
   inherit (nixpkgs-mozilla.lib) genAttrs;
 
   channels = [ "nightly" "beta" "stable" ];
   parse = file: builtins.fromJSON (builtins.readFile file);
   spec = genAttrs channels (channel: parse (./latest + "/${channel}.json"));
-in with nixpkgs-mozilla;
+in
+  with nixpkgs-mozilla;
   genAttrs channels
-    (channel: (rustChannelOf (spec.${channel} // { inherit channel; })) // {
-      inherit (nixpkgs-mozilla.rustPlatform) buildRustPackage;
-    })
+    (
+      channel: (rustChannelOf (spec.${channel} // { inherit channel; })) // {
+        inherit (nixpkgs-mozilla.rustPlatform) buildRustPackage;
+      }
+    )
