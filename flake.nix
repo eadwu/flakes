ab9fde36d3fdc0d8ea0a55e8b0f2f4c233c384fe
Edmund Wu <fangkazuto@gmail.com>
2020-04-10 17:59:34 -0400

    flake.nix: add liberation-mono alias
    Alias to the Nerd Font Liberation Mono

--- flake.nix
+++ flake.nix
@@ -28,6 +28,8 @@
             callPackage = pkgs.newScope { inherit pkgs; callPackage = _callPackage; };
           in (
             rec {
+              liberation-mono = pkgs.nerdfonts.override { fonts = [ "LiberationMono" ]; };
+
               boxpub = import inputs.boxpub { inherit system; nixpkgs = inputs.stable; };
               cachix = import inputs.cachix { inherit system; };
 
