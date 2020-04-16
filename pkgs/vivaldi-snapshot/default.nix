581e05c0407c8ef34f66616f9db4b3bde20eb106
Edmund Wu <fangkazuto@gmail.com>
2020-04-16 11:25:10 -0400

    vivaldi-snapshot: 3.0.1874.3 -> 3.0.1874.5

--- pkgs/vivaldi-snapshot/default.nix
+++ pkgs/vivaldi-snapshot/default.nix
@@ -15,11 +15,11 @@
 ).overrideAttrs (
   oldAttrs: rec {
     pname = "vivaldi-snapshot";
-    version = "3.0.1874.3";
+    version = "3.0.1874.5";
 
     src = fetchurl {
       url = "https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_${version}-1_amd64.deb";
-      sha256 = "0bg105yjcvph3cbmj4pcwck9383zks22nh1lcaw8flz6s5lb26pi";
+      sha256 = "1x81x9ghn66shb928zglpd3m3nh4wqgdkrdr76yr08jlzywbivzb";
     };
 
     newLibPath = lib.makeLibraryPath buildInputs
