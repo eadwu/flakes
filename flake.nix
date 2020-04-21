56dc57fba7fd5817c4162f754b32bcae2a765a7b
Edmund Wu <fangkazuto@gmail.com>
2020-04-20 20:14:20 -0400

    boxpub: use nixpkgs from source

--- flake.nix
+++ flake.nix
@@ -6,7 +6,6 @@
   inputs.boxpub = { type = "github"; owner = "eadwu"; repo = "boxpub"; flake = false; };
   inputs.nix-linter = { type = "github"; owner = "eadwu"; repo= "nix-linter"; flake = false; };
   inputs.plymouth-themes = { type = "github"; owner = "eadwu"; repo = "plymouth-themes"; flake = false; };
-  inputs.stable = { type = "github"; owner = "NixOS"; repo = "nixpkgs"; flake = false; ref = "0abc66e252ea7f11c18845a79cbfc59335356543"; };
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
@@ -33,7 +32,7 @@
 
               liberation-mono = pkgs.nerdfonts.override { fonts = [ "LiberationMono" ]; };
 
-              boxpub = import inputs.boxpub { inherit system; nixpkgs = inputs.stable; };
+              boxpub = import inputs.boxpub { inherit system; };
               cachix = import inputs.cachix { inherit system; };
               nix-linter = (import inputs.nix-linter { inherit args; }).nix-linter;
 
