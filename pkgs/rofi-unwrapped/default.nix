8269d0c67e1dbfb4f2bfa5e8fb58c30a3906bfb2
Edmund Wu <fangkazuto@gmail.com>
2020-04-30 12:44:42 -0400

    rofi-unwrapped: 1588157989 -> 1588230568

--- pkgs/rofi-unwrapped/default.nix
+++ pkgs/rofi-unwrapped/default.nix
@@ -24,13 +24,13 @@
 
 rofi-unwrapped.overrideAttrs (
   _: rec {
-    version = "1588157989";
+    version = "1588230568";
 
     src = fetchFromGitHub {
       owner = "davatorium";
       repo = "rofi";
-      rev = "86cdcae55b4e8cbfaa3bbcd0c65eb5ea926a096d";
-      sha256 = "0vvk8zdgnr8jmsmnlwwndkq6zcy2nwyw9iy8a0wyr0mic8p3jpby";
+      rev = "80fc8e16b2c4294e7ee282f7e1f5bce88c3129d7";
+      sha256 = "15kisl850yqpi391yai7cl662xf3spiqir4q027vcbg188jj24y7";
       fetchSubmodules = true;
     };
 
