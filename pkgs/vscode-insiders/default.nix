ade0128ab356a39fc48641660f1c71c9d6c2b1ea
Edmund Wu <fangkazuto@gmail.com>
2020-04-30 12:43:55 -0400

    vscode-insiders: 1588139461 -> 1588226274

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -7,11 +7,11 @@
 ).overrideAttrs (
   _: rec {
     pname = "vscode-insiders";
-    version = "1588139461";
+    version = "1588226274";
 
     src = fetchurl {
-      url = "https://az764295.vo.msecnd.net/insider/4bd206856db30c27d38aa0f1fbe74bac6156edc7/code-insider-1588139461.tar.gz";
-      sha256 = "1ksp70cdiqwaxlx50c613xkbfaaszzin3dgghbk9sqb9djsbbpg8";
+      url = "https://az764295.vo.msecnd.net/insider/a0fe37870c42e0704a492cdc6b8550b4cf23f63c/code-insider-1588226274.tar.gz";
+      sha256 = "0572njdpzmvs9hd3mc9kzgj3cyawkvldg912g49pic2yxpi5pwws";
     };
   }
 )
