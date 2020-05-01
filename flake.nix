507da7c2ca0ff09c66077c940cdef2d60eb7ad31
Edmund Wu <fangkazuto@gmail.com>
2020-05-01 01:19:29 -0400

    nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
   inputs.rolling = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "rolling"; };
 
   outputs = { self, nixpkgs, ... }@inputs: with nixpkgs.lib; let
-    _foldl' = f: foldl recursiveUpdate {} (map f (attrValues (removeAttrs inputs [ "self" "nixpkgs" ])));
+    _foldl' = f: foldl recursiveUpdate { } (map f (attrValues (removeAttrs inputs [ "self" "nixpkgs" ])));
   in
-    _foldl' (f: f.outputs)
-    // {
-      overlays = system: self: super: _foldl' (f: f.overlays system self super);
-    };
+  _foldl' (f: f.outputs)
+  // {
+    overlays = system: self: super: _foldl' (f: f.overlays system self super);
+  };
 }
