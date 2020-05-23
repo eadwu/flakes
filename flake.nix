5c06ce154abbcf3d6998e7215f9fc8bc94df6a55
Edmund Wu <fangkazuto@gmail.com>
2020-05-22 21:38:02 -0400

    flake.nix: convert to overlay based structure
    Overlays are quite composable in nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,89 +11,110 @@
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      lib = nixpkgs.lib;
-      systems = [ "x86_64-linux" ];
-      forAllSystems = f: lib.genAttrs systems (system: f system);
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; config.allowUnfree = true; });
     in
-    rec {
-      overlays = system: final: prev: lib.genAttrs
-        (builtins.attrNames (packages.${system}))
-        (package: packages.${system}.${package});
+    {
+      overlay = final: prev: with final.pkgs; {
+        kernelPatches = prev.kernelPatches // (callPackage ./pkgs/kernel/patches.nix { });
 
-      packages = forAllSystems (
-        system:
-        let
-          args = {
-            inherit system;
-            config.allowUnfree = true;
-          };
-          pkgs = import nixpkgs args;
-          inherit (pkgs) callPackage;
-        in
-        (
-          rec {
-            kernelPatches = pkgs.kernelPatches // (callPackage ./pkgs/kernel/patches.nix { });
+        liberation-mono = prev.nerdfonts.override { fonts = [ "LiberationMono" ]; };
 
-            liberation-mono = pkgs.nerdfonts.override { fonts = [ "LiberationMono" ]; };
+        boxpub = import inputs.boxpub { inherit (stdenv.hostPlatform) system; };
+        cachix = import inputs.cachix { inherit (stdenv.hostPlatform) system; };
+        nixopsUnstable = (import inputs.nixops { inherit (final) pkgs; inherit (inputs) nixpkgs; })
+          .overrideAttrs (_: { postInstall = ""; });
+        nix-linter = (import inputs.nix-linter { args = { inherit (stdenv.hostPlatform) system; }; })
+          .nix-linter;
 
-            boxpub = import inputs.boxpub { inherit system; };
-            cachix = import inputs.cachix { inherit system; };
-            nixopsUnstable = (import inputs.nixops { inherit pkgs;inherit (inputs) nixpkgs; }).overrideAttrs (_: { postInstall = ""; });
-            nix-linter = (import inputs.nix-linter { inherit args; }).nix-linter;
+        clight-modules = callPackage ./pkgs/clight-modules { };
 
-            clight-modules = callPackage ./pkgs/clight-modules { };
+        i3lock-color = prev.i3lock-color.overrideAttrs (
+          oldAttrs: {
+            # Don't include ASAN runtime in release
+            postPatch = (oldAttrs.postPatch or "") + ''
+              sed -i 's/is_release=.*/is_release=yes/g' configure.ac
+            '';
+          }
+        );
 
-            i3lock-color = callPackage ./pkgs/i3lock-color { };
+        picom = prev.picom.overrideAttrs (
+          oldAttrs: {
+            version = "2020-04-24";
 
-            picom = callPackage ./pkgs/picom { };
-
-            radian = let
-              packageOverrides = final: prev: {
-                lineedit = self.callPackage ./pkgs/lineedit { };
-                rchitect = self.callPackage ./pkgs/rchitect { };
-                radian = self.callPackage ./pkgs/radian { };
-              };
-              python = pkgs.python3.override { inherit packageOverrides; };
-            in python.pkgs.radian;
-
-            plymouth-themes = import inputs.plymouth-themes { inherit pkgs; };
-            dual-plymouth-theme = callPackage ./pkgs/dual-plymouth-theme {
-              inherit (plymouth-themes) mkTheme;
-              boot = plymouth-themes."1891042977";
-              shutdown = plymouth-themes."1987238292";
+            src = fetchFromGitHub {
+              owner = "yshui";
+              repo = "picom";
+              rev = "0b377537ec9c3f6faaa13878701d8d0b2ee62d0c";
+              sha256 = "0z4iw2ypi3r4hrqxci9yi46acfpc7q35b0srlbrba4wznpfziagm";
+              fetchSubmodules = true;
             };
 
-            rtLinuxPackagesFor = kernel: pkgs.linuxPackagesFor (kernel.override {
-              structuredExtraConfig = with lib.kernel; {
-                PREEMPT = yes;
-                PREEMPT_VOLUNTARY = option no;
-              };
-              kernelPatches = kernel.kernelPatches
-                ++ [
-                kernelPatches.rt
-              ];
-              modDirVersionArg = kernel.modDirVersion;
-            });
-
-            customLinuxPackagesFor = kernel: pkgs.linuxPackagesFor (kernel.override {
-              structuredExtraConfig = { };
-              kernelPatches = kernel.kernelPatches
-                ++ [
-                kernelPatches.o3
-                kernelPatches.bmq
-                kernelPatches.xanmod
-                kernelPatches.zen-zswap
-                kernelPatches.enable-fsgsbase-instructions
-                kernelPatches.extra_config
-              ];
-              modDirVersionArg = kernel.modDirVersion;
-            });
-
-            linuxPackages_custom = customLinuxPackagesFor (rtLinuxPackagesFor (pkgs.linux_latest_hardened)).kernel;
-            linux_custom = linuxPackages_custom.kernel;
+            patches = (oldAttrs.patches or [ ])
+              ++ [ ./pkgs/picom/dual_kawase.patch.xz ];
           }
-        )
-      );
+        );
+
+        radian =
+          let
+            packageOverrides = final: prev: with final; {
+              lineedit = callPackage ./pkgs/lineedit { };
+              rchitect = callPackage ./pkgs/rchitect { };
+              radian = callPackage ./pkgs/radian { };
+            };
+            python = python3.override { inherit packageOverrides; };
+          in
+          python.pkgs.radian;
+
+        plymouth-themes = import inputs.plymouth-themes { inherit (self) pkgs; };
+        dual-plymouth-theme = callPackage ./pkgs/dual-plymouth-theme {
+          inherit (plymouth-themes) mkTheme;
+          boot = plymouth-themes."1891042977";
+          shutdown = plymouth-themes."1987238292";
+        };
+
+        rtLinuxPackagesFor = kernel: linuxPackagesFor (kernel.override {
+          structuredExtraConfig = with nixpkgs.lib.kernel; {
+            PREEMPT = yes;
+            PREEMPT_VOLUNTARY = option no;
+          };
+          kernelPatches = kernel.kernelPatches
+            ++ [
+            kernelPatches.rt
+          ];
+          modDirVersionArg = kernel.modDirVersion;
+        });
+
+        customLinuxPackagesFor = kernel: linuxPackagesFor (kernel.override {
+          structuredExtraConfig = { };
+          kernelPatches = kernel.kernelPatches
+            ++ [
+            kernelPatches.o3
+            kernelPatches.bmq
+            kernelPatches.xanmod
+            kernelPatches.zen-zswap
+            kernelPatches.enable-fsgsbase-instructions
+            kernelPatches.extra_config
+          ];
+          modDirVersionArg = kernel.modDirVersion;
+        });
+
+        linuxPackages_custom = customLinuxPackagesFor (rtLinuxPackagesFor linux_latest_hardened).kernel;
+        linux_custom = linuxPackages_custom.kernel;
+      };
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system})
+          liberation-mono
+          boxpub cachix nixopsUnstable nix-linter
+          clight-modules i3lock-color picom radian
+          plymouth-themes dual-plymouth-theme
+          linuxPackages_custom linux_custom
+          ;
+      });
+
+      checks = forAllSystems (system: self.packages.${system});
 
       nixosModules = {
         apropos = import ./modules/apropos.nix;
