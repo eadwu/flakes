e79d81e63e281c0b9ed1e7ad842e7a6c4ef5d0bc
Edmund Wu <fangkazuto@gmail.com>
2020-06-09 22:06:06 -0400

    discord-canary: fix executable rpath

--- pkgs/discord-canary/default.nix
+++ pkgs/discord-canary/default.nix
@@ -1,7 +1,9 @@
-{ fetchurl, discord-canary }:
+{ fetchurl, discord-canary
+, autoPatchelfHook, atomEnv
+}:
 
 discord-canary.overrideAttrs (
-  _: rec {
+  oldAttrs: rec {
     pname = "discord";
     version = "0.0.104";
 
@@ -9,5 +11,11 @@ discord-canary.overrideAttrs (
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
       sha256 = "17np1hqqygjlbmlln0d1ba2qlbjykwj156w5dw7g4lg77kfxicfk";
     };
+
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
+      autoPatchelfHook
+    ];
+
+    buildInputs = (oldAttrs.buildInputs or [ ]) ++ atomEnv.packages;
   }
 )
