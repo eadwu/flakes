461ef347451de06c6e752cb7213c2eb8f69e9026
actions <actions@eadwu.flakes>
2020-07-05 15:07:42 +0000

    rofi-unwrapped: 1593957639 -> 1593958075

--- pkgs/rofi-unwrapped/default.nix
+++ pkgs/rofi-unwrapped/default.nix
@@ -24,13 +24,13 @@
 
 rofi-unwrapped.overrideAttrs (
   _: rec {
-    version = "1593957639";
+    version = "1593958075";
 
     src = fetchFromGitHub {
       owner = "davatorium";
       repo = "rofi";
-      rev = "72138c1a1025c76f762e9fa8222a3a59ef1c0475";
-      sha256 = "0gzs8g72g16cih2306fnm5ipjsns37s66s2ai5aibl2s56nvnsjg";
+      rev = "f93033ebaa3a0a203828ded46318ba1db48f156c";
+      sha256 = "1656zkkdwcm6shszlcfy1gnnv4qzh7wni0jrfrzz0lzmmbjb6112";
       fetchSubmodules = true;
     };
 
