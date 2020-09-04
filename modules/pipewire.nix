7a6c2317dc84598478caaf132f11d220048c3456
Edmund Wu <fangkazuto@gmail.com>
2020-09-04 16:48:09 -0400

    modules/pipewire: update configuration
    Include pipewire into udev
    Use custom asound configuration, pulled from nixpkgs#93725
    Include overlays used locally in module

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -2,7 +2,9 @@
 
 with lib;
 
-{
+let
+  cfg = config.services.pipewire;
+in {
   options.services.pipewire = {
     configFile = mkOption {
       type = types.path;
@@ -10,13 +12,14 @@ with lib;
     };
   };
 
-  config = mkIf config.services.pipewire.enable {
+  config = mkIf cfg.enable {
     boot.kernelModules = [ "snd_seq" "snd_seq_midi" ];
-    environment.systemPackages = with pkgs; [ pipewire.lib ];
+    environment.systemPackages = [ pkgs.pipewire.lib ];
 
     sound.enable = true;
     security.rtkit.enable = true;
-    systemd.user.services.pipewire.path = with pkgs; [ pipewire ];
+    services.udev.packages = [ pkgs.pipewire ];
+    systemd.user.services.pipewire.path = [ pkgs.pipewire ];
 
     environment.etc = {
       "asound.conf".source = pkgs.writeText "asound.conf" ''
@@ -25,14 +28,71 @@ with lib;
 
         <${pkgs.pipewire}/share/alsa/alsa.conf.d/50-pipewire.conf>
         pcm_type.pipewire {
-          libs.native = ${pkgs.pipewire.lib}/lib/alsa-lib/libasound_module_pcm_pipewire.so
+          libs.native = ${pkgs.pipewire.lib}/lib/alsa-lib/libasound_module_pcm_pipewire.so ;
         }
-        <${pkgs.pipewire}/share/alsa/alsa.conf.d/99-pipewire-default.conf>
+        pcm.!default {
+          @func getenv
+          vars [ PCM ]
+          default "plug:pipewire"
+          playback_mode "-1"
+          capture_mode "-1"
+        }
 
         ${config.sound.extraConfig}
       '';
 
       "pipewire/pipewire.conf".source = config.services.pipewire.configFile;
     };
+
+    nixpkgs.overlays = [
+      (final: prev:
+        with final;
+        {
+
+          pipewire = prev.pipewire.overrideAttrs (oldAttrs: {
+            postPatch = (oldAttrs.postPatch or "") + ''
+              sed -i 's@PIPEWIRE_CONFIG_DIR.*@\"/etc/pipewire/pipewire.conf\"@' src/daemon/daemon-config.c
+            '';
+
+            buildInputs = (oldAttrs.buildInputs or []) ++ [
+              xmltoman
+            ];
+
+            mesonFlags = (oldAttrs.mesonFlags or []) ++ [
+              "-Ddocs=true"
+
+              "-Dlibjack-path=${placeholder "lib"}/lib"
+              "-Dlibpulse-path=${placeholder "lib"}/lib"
+
+              "-Daudiotestsrc=true"
+              "-Dffmpeg=true"
+              "-Dtest=true"
+              "-Dvideotestsrc=true"
+              "-Dvolume=true"
+            ];
+          });
+
+          xmltoman = stdenv.mkDerivation rec {
+            pname = "xmltoman";
+            version = "0.4";
+
+            src = fetchurl {
+              url = "https://downloads.sourceforge.net/project/${pname}/${pname}/${pname}-${version}.tar.gz/${pname}-${version}.tar.gz";
+              sha256 = "1c0lvzr7kdy63wbn1jv6s126ds7add3pxqb0vlxd3v5a2sir91wl";
+            };
+
+            postPatch = ''
+              patchShebangs xmltoman
+            '';
+
+            buildInputs = [ perlPackages.perl perlPackages.XMLParser ];
+
+            makeFlags = [
+              "PREFIX=${placeholder "out"}"
+            ];
+          };
+
+        })
+    ];
   };
 }
