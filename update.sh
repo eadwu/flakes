dd5ee2af4bda0edf11b414c8aacb281f248fbb4a
Edmund Wu <fangkazuto@gmail.com>
2020-11-09 20:45:15 -0500

    update.sh: ignore dwm and st
    Causing a lot of failed builds when updating

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p coreutils
 
-for f in `find -mindepth 2 -name update.sh`; do
+for f in `find -mindepth 2 -name update.sh | grep -v "/dwm/" | grep -v "/st/"`; do
   dir="$(dirname "$f")"
   echo "running update script in $dir ... "
   "$f"
