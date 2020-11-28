2a50c74332c8c18c8c8f5f5d6cf3e6601a02489d
Edmund Wu <fangkazuto@gmail.com>
2020-11-28 14:22:21 -0500

    pipewire: 2020-11-27 -> 2020-11-28

--- pkgs/pipewire/default.nix
+++ pkgs/pipewire/default.nix
@@ -2,13 +2,13 @@
 
 pipewire.overrideAttrs (_:
   {
-    version = "2020-11-27";
+    version = "2020-11-28";
 
     src = fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "pipewire";
       repo = "pipewire";
-      rev = "77791bd427a20eeed601e02a8a197eaccdbf9c7f";
-      sha256 = "0vc4xdw6dmqfxndljqqxn5dwm7ql7v2rbhbnrmii26bx7kgby18m";
+      rev = "62cdec84481d74c4d54bbcf3717f09e5edaf455c";
+      sha256 = "1d6yzk3q9i0aqcq528sn93d4q97kaarxzmqnh37karwcdwfqkq10";
     };
   })
