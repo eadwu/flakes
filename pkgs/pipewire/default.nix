a10b016a483cee5ddcc3ff1d23064c14f58c2fdf
Edmund Wu <fangkazuto@gmail.com>
2020-12-02 11:52:39 -0500

    pipewire: 2020-12-02 -> 1606920176

--- pkgs/pipewire/default.nix
+++ pkgs/pipewire/default.nix
@@ -2,13 +2,13 @@
 
 (pipewire.override { hsphfpdSupport = true; }).overrideAttrs (_:
   {
-    version = "2020-12-02";
+    version = "1606920176";
 
     src = fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "pipewire";
       repo = "pipewire";
-      rev = "4c486c7ae322fbcc903884a30da72552a1b57f07";
-      sha256 = "1dqb628b1k246pw3hc1bqqgjxi31a55paz71llfw0dm3xsw4c55x";
+      rev = "3c2ab98a2a0ab48f087c375ec9ff5a69b0c64dcf";
+      sha256 = "187vwfg4bi9rwi6ngi1i0jaslpl4k7w8ksc0r1m6px0zccvpzliw";
     };
   })
