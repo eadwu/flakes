9506b50c58e5ab5eadeb85adcb085df1190a8ee2
actions <actions@eadwu.flakes>
2020-12-05 12:32:57 +0000

    pipewire: 1607153414 -> 1607171402

--- pkgs/pipewire/default.nix
+++ pkgs/pipewire/default.nix
@@ -2,13 +2,13 @@
 
 pipewire.overrideAttrs (_:
   {
-    version = "1607153414";
+    version = "1607171402";
 
     src = fetchFromGitLab {
       domain = "gitlab.freedesktop.org";
       owner = "pipewire";
       repo = "pipewire";
-      rev = "c3c1a1184c0cb051b39c720525d13dc1cd484b1d";
-      sha256 = "1xhqzwcz1rxxg0ig6cmynwyzwb04726srx972mgrhbg4gr3bwz4c";
+      rev = "cfe43cbea746aa09de979be2972350071a4921c9";
+      sha256 = "1nk06h19hqa57jxzinxv5qakhwvikadh8qg1l6a27595cinlgj69";
     };
   })
