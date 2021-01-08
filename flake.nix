0e6bef36e0ef97bd9bdff4e582a0829039faad61
Edmund Wu <fangkazuto@gmail.com>
2021-01-08 17:47:42 -0500

    flake.nix: inline input configuration

--- flake.nix
+++ flake.nix
@@ -1,11 +1,8 @@
 {
   description = "A collection of Nix flakes";
 
-  inputs.custom = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "custom"; };
-  inputs.rolling = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "rolling"; };
-
-  inputs.custom.inputs.nixpkgs.follows = "/nixpkgs";
-  inputs.rolling.inputs.nixpkgs.follows = "/nixpkgs";
+  inputs.custom = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "custom"; inputs.nixpkgs.follows = "/nixpkgs"; };
+  inputs.rolling = { type = "github"; owner = "eadwu"; repo = "flakes"; ref = "rolling"; inputs.nixpkgs.follows = "/nixpkgs"; };
 
   outputs = { self, nixpkgs, ... }@inputs: with nixpkgs.lib; let
     _foldl' = f: foldl recursiveUpdate { } (map f (attrValues (removeAttrs inputs [ "self" "nixpkgs" ])));
