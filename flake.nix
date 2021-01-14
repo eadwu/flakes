0d8ec7a625cf71014162045971d2dbdcc873fc09
Edmund Wu <fangkazuto@gmail.com>
2021-01-13 20:19:57 -0500

    flake.nix: include emacs-overlay

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
   inputs.boxpub = { type = "github"; owner = "eadwu"; repo = "boxpub"; ref = "boxpub/2.x"; flake = false; };
   inputs.cachix = { type = "github"; owner = "eadwu"; repo = "cachix"; flake = false; };
+  inputs.emacs-overlay = { type = "github"; owner = "nix-community"; repo = "emacs-overlay"; flake = false; };
   inputs.nixops = { type = "github"; owner = "NixOS"; repo = "nixops"; };
   inputs.nix-linter = { type = "github"; owner = "eadwu"; repo = "nix-linter"; flake = false; };
   inputs.plymouth-themes = { type = "github"; owner = "eadwu"; repo = "plymouth-themes"; flake = false; };
@@ -22,111 +23,122 @@
     let
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; config.allowUnfree = true; });
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; inherit (self) overlays; config.allowUnfree = true; });
     in
-    {
-      overlay = final: prev: with final.pkgs; {
-        kernelPatches = prev.kernelPatches // (callPackage ./pkgs/kernel/patches.nix { });
+      {
+        overlay = self.overlays.default;
 
-        ladspa-bs2b = callPackage ./pkgs/ladspa-bs2b { };
-        liberation-mono = prev.nerdfonts.override { fonts = [ "LiberationMono" ]; };
+        overlays.emacs-overlay = final: prev: import inputs.emacs-overlay final prev;
+        overlays.default = final: prev: with final.pkgs; {
+          kernelPatches = prev.kernelPatches // (callPackage ./pkgs/kernel/patches.nix {});
 
-        boxpub = import inputs.boxpub { inherit (stdenv.hostPlatform) system; };
-        cachix = import inputs.cachix { inherit (stdenv.hostPlatform) system; };
-        nixopsUnstable = nixops.defaultPackage.${system};
-        nix-linter = (import inputs.nix-linter { args = { inherit (stdenv.hostPlatform) system; }; }).nix-linter;
+          ladspa-bs2b = callPackage ./pkgs/ladspa-bs2b {};
+          liberation-mono = prev.nerdfonts.override { fonts = [ "LiberationMono" ]; };
 
-        clight-modules = callPackage ./pkgs/clight-modules { };
+          boxpub = import inputs.boxpub { inherit (stdenv.hostPlatform) system; };
+          cachix = import inputs.cachix { inherit (stdenv.hostPlatform) system; };
+          nixopsUnstable = nixops.defaultPackage.${system};
+          nix-linter = (import inputs.nix-linter { args = { inherit (stdenv.hostPlatform) system; }; }).nix-linter;
 
-        i3lock-color = prev.i3lock-color.overrideAttrs (
-          oldAttrs: {
-            # Don't include ASAN runtime in release
-            postPatch = (oldAttrs.postPatch or "") + ''
-              sed -i 's/is_release=.*/is_release=yes/g' configure.ac
-            '';
-          }
-        );
+          clight-modules = callPackage ./pkgs/clight-modules {};
 
-        plymouth-themes = import inputs.plymouth-themes { inherit (final) pkgs; };
-        dual-plymouth-theme = callPackage ./pkgs/dual-plymouth-theme {
-          inherit (plymouth-themes) mkTheme;
-          boot = plymouth-themes."1891042977";
-          shutdown = plymouth-themes."1987238292";
+          i3lock-color = prev.i3lock-color.overrideAttrs (
+            oldAttrs: {
+              # Don't include ASAN runtime in release
+              postPatch = (oldAttrs.postPatch or "") + ''
+                sed -i 's/is_release=.*/is_release=yes/g' configure.ac
+              '';
+            }
+          );
+
+          plymouth-themes = import inputs.plymouth-themes { inherit (final) pkgs; };
+          dual-plymouth-theme = callPackage ./pkgs/dual-plymouth-theme {
+            inherit (plymouth-themes) mkTheme;
+            boot = plymouth-themes."1891042977";
+            shutdown = plymouth-themes."1987238292";
+          };
+
+          rtLinuxPackagesFor = kernel: linuxPackagesFor (
+            kernel.override {
+              structuredExtraConfig = with nixpkgs.lib;
+                (mapAttrs (_: v: mkForce v) kernel.configfile.moduleStructuredConfig.settings)
+                // (
+                  with nixpkgs.lib.kernel; {
+                    # Preempt
+                    EXPERT = yes;
+                    PREEMPT = yes;
+                    PREEMPT_VOLUNTARY = mkOverride 36 no;
+                    IRQ_FORCED_THREADING = yes;
+                    RT_GROUP_SCHED = mkOverride 36 (option no);
+
+                    # RCU
+                    RCU_EXPERT = yes;
+                    RCU_BOOST = yes;
+                    RCU_BOOST_DELAY = freeform "0";
+                    RCU_NOCB_CPU = yes;
+
+                    # Timer
+                    HZ_100 = yes;
+                    HZ_250 = mkOverride 36 no;
+                    HZ_1000 = mkOverride 36 no;
+                    HZ = mkOverride 36 (freeform "100");
+                  }
+                );
+              kernelPatches = kernel.kernelPatches;
+              modDirVersionArg = kernel.modDirVersion;
+            }
+          );
+
+          customLinuxPackagesFor = kernel: linuxPackagesFor (
+            kernel.override {
+              structuredExtraConfig = with nixpkgs.lib;
+                mapAttrs (_: v: mkForce v) kernel.configfile.moduleStructuredConfig.settings;
+              kernelPatches = kernel.kernelPatches ++ [
+                kernelPatches.o3
+                kernelPatches.clearlinux.default
+                kernelPatches.zen-tweaks
+                kernelPatches.extra-config
+              ];
+              modDirVersionArg = kernel.modDirVersion;
+            }
+          );
+
+          linuxPackages_custom = customLinuxPackagesFor (rtLinuxPackagesFor linux_latest).kernel;
+          linux_custom = linuxPackages_custom.kernel;
         };
 
-        rtLinuxPackagesFor = kernel: linuxPackagesFor (kernel.override {
-          structuredExtraConfig = with nixpkgs.lib;
-            (mapAttrs (_: v: mkForce v) kernel.configfile.moduleStructuredConfig.settings)
-            // (with nixpkgs.lib.kernel; {
-              # Preempt
-              EXPERT = yes;
-              PREEMPT = yes;
-              PREEMPT_VOLUNTARY = mkOverride 36 no;
-              IRQ_FORCED_THREADING = yes;
-              RT_GROUP_SCHED = mkOverride 36 (option no);
+        packages = forAllSystems (
+          system:
+            let
+              pkgSet = nixpkgsFor.${system};
+            in
+              {
+                inherit (pkgSet)
+                  ladspa-bs2b liberation-mono
+                  boxpub cachix nixopsUnstable nix-linter
+                  i3lock-color
+                  dual-plymouth-theme
+                  linux_custom
+                  ;
 
-              # RCU
-              RCU_EXPERT = yes;
-              RCU_BOOST = yes;
-              RCU_BOOST_DELAY = freeform "0";
-              RCU_NOCB_CPU = yes;
+                inherit (pkgSet.clight-modules)
+                  inhibit_bl trendlog
+                  ;
 
-              # Timer
-              HZ_100 = yes;
-              HZ_250 = mkOverride 36 no;
-              HZ_1000 = mkOverride 36 no;
-              HZ = mkOverride 36 (freeform "100");
-            });
-          kernelPatches = kernel.kernelPatches;
-          modDirVersionArg = kernel.modDirVersion;
-        });
+                inherit (pkgSet.plymouth-themes)
+                  geshin-impact-start
+                  ;
+              }
+        );
 
-        customLinuxPackagesFor = kernel: linuxPackagesFor (kernel.override {
-          structuredExtraConfig = with nixpkgs.lib;
-            mapAttrs (_: v: mkForce v) kernel.configfile.moduleStructuredConfig.settings;
-          kernelPatches = kernel.kernelPatches ++ [
-            kernelPatches.o3
-            kernelPatches.clearlinux.default
-            kernelPatches.zen-tweaks
-            kernelPatches.extra-config
-          ];
-          modDirVersionArg = kernel.modDirVersion;
-        });
+        checks = forAllSystems (system: self.packages.${system});
 
-        linuxPackages_custom = customLinuxPackagesFor (rtLinuxPackagesFor linux_latest).kernel;
-        linux_custom = linuxPackages_custom.kernel;
+        nixosModules = {
+          r-36 = import ./modules/r-36.nix inputs;
+          custom = import ./modules/custom.nix inputs;
+          pipewire = import ./modules/pipewire.nix;
+          sourcehut = import ./modules/sourcehut;
+          pam-keyring = import ./modules/pam-keyring.nix;
+        };
       };
-
-      packages = forAllSystems (system:
-        let
-          pkgSet = nixpkgsFor.${system};
-        in
-        {
-          inherit (pkgSet)
-            ladspa-bs2b liberation-mono
-            boxpub cachix nixopsUnstable nix-linter
-            i3lock-color
-            dual-plymouth-theme
-            linux_custom
-            ;
-
-          inherit (pkgSet.clight-modules)
-            inhibit_bl trendlog
-            ;
-
-          inherit (pkgSet.plymouth-themes)
-            geshin-impact-start
-            ;
-        });
-
-      checks = forAllSystems (system: self.packages.${system});
-
-      nixosModules = {
-        r-36 = import ./modules/r-36.nix inputs;
-        custom = import ./modules/custom.nix inputs;
-        pipewire = import ./modules/pipewire.nix;
-        sourcehut = import ./modules/sourcehut;
-        pam-keyring = import ./modules/pam-keyring.nix;
-      };
-    };
 }
