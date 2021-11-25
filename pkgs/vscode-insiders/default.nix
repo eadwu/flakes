7f6e5605ed74f8fd4e3f6f00b1083a8741831e83
Edmund Wu <fangkazuto@gmail.com>
2021-11-24 21:29:23 -0500

    vscode-insiders: skip postPatch step

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -15,5 +15,7 @@ in
       url = "https://az764295.vo.msecnd.net/insider/c1f2f40bcd81380bf2e286af64becddada57db88/code-insider-x64-1637742447.tar.gz";
       sha256 = "0dgpf5rlm1vsyrhcip87f71h4gxcinc7glq8pha5wakf9vbbsn3n";
     };
+
+    postPatch = "";
   }
 )
