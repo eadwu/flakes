9c9c795d1f865dbfd6389486c7d33ba3f2c0b30d
actions <actions@eadwu.flakes>
2022-06-02 04:39:49 +0000

    vscode-insiders:  -> 1654070943

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -16,6 +16,6 @@ in
       sha256 = "10kkpx0f8dxigd3igs8narf6zgflwf07pynfwbswqpwj361a78zl";
     };
 
-    postPatch = "";
+    postPatch = "https://az764295.vo.msecnd.net/insider/4589815e4849499c67125ff68563fa102646b869/code-insider-x64-1654070943.tar.gz";
   }
 )
