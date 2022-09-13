b43d2afebed57e6276cf945f2f2748a7e4ebdb65
actions <actions@eadwu.flakes>
2022-09-13 12:21:11 +0000

    vivaldi-snapshot: 5.5.2770.3 -> 5.5.2794.3

--- pkgs/vivaldi-snapshot/default.nix
+++ pkgs/vivaldi-snapshot/default.nix
@@ -17,11 +17,11 @@
 ).overrideAttrs (
   oldAttrs: rec {
     pname = "vivaldi-snapshot";
-    version = "5.5.2770.3";
+    version = "5.5.2794.3";
 
     src = fetchurl {
       url = "https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_${version}-1_amd64.deb";
-      sha256 = "17wyvi51sc37rbkd8nrpbmwfxagns7njxd508sxdbaa8k4a6s242";
+      sha256 = "01j68bxx8237rii56vdrysb07msk82532c5p269i9gvhkrznh3l9";
     };
 
     newLibPath = lib.makeLibraryPath buildInputs
