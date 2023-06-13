c2ea3444efb7df6cffd00005249b193d656f2e56
Edmund Wu <fangkazuto@gmail.com>
2023-06-13 17:32:53 -0400

    vivaldi-snapshot: remove library patch

--- pkgs/vivaldi-snapshot/default.nix
+++ pkgs/vivaldi-snapshot/default.nix
@@ -23,31 +23,5 @@
       url = "https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_${version}-1_amd64.deb";
       sha256 = "16yqm5pcsn6ljbpz7ncv1zcg41rcjxgdcn0a93b8gaypkfjk23mw";
     };
-
-    newLibPath = lib.makeLibraryPath buildInputs
-      + (":" + lib.makeSearchPathOutput "lib" "lib64" buildInputs)
-      + ":$out/opt/vivaldi-snapshot/lib";
-
-    buildInputs = oldAttrs.buildInputs ++
-      [
-        mesa
-        libdrm
-        libxkbcommon
-        libxshmfence
-      ];
-
-    buildPhase = oldAttrs.buildPhase + ''
-      patchelf \
-        --set-rpath "${newLibPath}" \
-        opt/vivaldi-snapshot/vivaldi-bin
-
-      for exe in $(find opt/vivaldi-snapshot -type f -executable); do
-        echo "Patching Vivaldi binary $exe"
-        # Can fail if not an ELF executable
-        patchelf \
-          --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-          "$exe" || true
-      done
-    '';
   }
 )
