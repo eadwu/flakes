2c627ad71c8b8bd69506c26f828a3fcc819dccb3
actions <actions@eadwu.flakes>
2023-11-11 07:09:42 +0000

    vscode-insiders:  -> 1699627551

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -9,10 +9,10 @@ in
 ).overrideAttrs (
   _: rec {
     pname = "vscode-insiders";
-    version = "";
+    version = "1699627551";
 
     src = fetchurl {
-      url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
+      url = "https://az764295.vo.msecnd.net/insider/2095b6f5a316c998fbfa17586d24f7974e3c8f13/code-insider-x64-1699627551.tar.gz";
       sha256 = "062m7vxdvlc36vjjlwjm48j8ybqbvzyapq8l7yxyz3vg5r3fvn3j";
     };
   }
