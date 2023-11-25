75dd2b71cbe4e2080760e4660a2107e68891d006
actions <actions@eadwu.flakes>
2023-11-25 10:09:07 +0000

    vivaldi-snapshot: 6.5.3189.3 -> 6.5.3201.4

--- pkgs/vivaldi-snapshot/default.nix
+++ pkgs/vivaldi-snapshot/default.nix
@@ -17,11 +17,11 @@
 ).overrideAttrs (
   oldAttrs: rec {
     pname = "vivaldi-snapshot";
-    version = "6.5.3189.3";
+    version = "6.5.3201.4";
 
     src = fetchurl {
       url = "https://downloads.vivaldi.com/snapshot/vivaldi-snapshot_${version}-1_amd64.deb";
-      sha256 = "1k6w1dg0dsigk8yf51qklv2d4k5yhzhk5chvs5c1kkxhm9ki56rg";
+      sha256 = "0rlha8cxqfnqmq0pdndnkfqmv2il41wjna5lswm9mkdr8sn0i3g9";
     };
   }
 )
