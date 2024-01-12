4e562121cb1aa9e365e38535c2460395372ba88e
actions <actions@eadwu.flakes>
2024-01-12 22:09:19 +0000

    discord-canary: 0.0.246 -> 0.0.248

--- pkgs/discord-canary/default.nix
+++ pkgs/discord-canary/default.nix
@@ -5,11 +5,11 @@
 discord-canary.overrideAttrs (
   oldAttrs: rec {
     pname = "discord";
-    version = "0.0.246";
+    version = "0.0.248";
 
     src = fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "0sqzl8k9bm592xwrs1g85c7l3vc3g26m4nk9iyc13fz5z7521362";
+      sha256 = "131lmfwdq0fbq1rv5j61xm24sf8m6qpqi0bbmf6l83ffx6zfa670";
     };
   }
 )
