e51efca4a10de64f40b02dcc39e09224b9a4a815
actions <actions@eadwu.flakes>
2025-05-13 17:14:56 +0000

    vscode-insiders: 1747113693 -> 1747125918

--- pkgs/vscode-insiders/default.nix
+++ pkgs/vscode-insiders/default.nix
@@ -9,11 +9,11 @@ in
 ).overrideAttrs (
   _: rec {
     pname = "vscode-insiders";
-    version = "1747113693";
+    version = "1747125918";
 
     src = fetchurl {
-      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/1250f01c2bd215b6d5d07d933a62c96df35da3ea/code-insider-x64-1747113693.tar.gz";
-      sha256 = "1k7658c4gcw4gq279ya2axjqc7hm0faswxvxdpvxcxiiazqd7h5x";
+      url = "https://vscode.download.prss.microsoft.com/dbazure/download/insider/4b88fa7949c13eea3369e8a05042a03f2f9df8be/code-insider-x64-1747125918.tar.gz";
+      sha256 = "176ybcc6gr172z30d60v61bmiad3yd7vyjc4pg8xlnpcrw6mgkcl";
     };
   }
 )
