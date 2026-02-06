666f80543322d42d9882a54d6d754ac88cb4b411
actions <actions@eadwu.flakes>
2026-02-06 22:39:23 +0000

    discord-canary: 0.0.862 -> 0.0.863

--- pkgs/discord-canary/default.nix
+++ pkgs/discord-canary/default.nix
@@ -5,7 +5,7 @@
 discord-canary.overrideAttrs (
   oldAttrs: rec {
     pname = "discord";
-    version = "0.0.862";
+    version = "0.0.863";
 
     src = fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
